bf6d47fd8237ac63eabdde1eeac75fc6b026c9e5
Aurelienazerty <aurelienwillem@hotmail.com>
2015-02-20 23:53:39 +0100

    1.0.0 dev
    Version fonctionnelle. Néanmoins le choix du logo pourrait être fait via
    une liste déroulante ou bien des boutons radio avec les logo.

---
+++ ext.php
@@ -0,0 +1,20 @@
+<?php
+/**
+*
+* @package Xiti marker.
+* @version $$
+* @copyright (c) 2015 www.team-azerty.com
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+namespace Aurelienazerty\xiti;
+
+/**
+* ext Class
+* www.team-azerty.com
+* @package Display Last Post
+*/
+class ext extends \phpbb\extension\base
+{
+}
